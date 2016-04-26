9bdf31c33f0c4900d682c91ccd0dc21eb0e90a1d
alee112 <alee112@terpmail.umd.edu>
2016-04-25 21:03:58 -0400

    master

--- control.py
+++ control.py
@@ -28,72 +28,98 @@ The Baxter World operators use two state variables:
 - holdingL and holdingR = name of the block being held in each hand, or False if the hand is empty.
 """
 
-
 def pickup(state, b, hand):
-    if hand == 'right' and (state.pos[b] == 't2' or state.pos[b] == 'center') and state.holdingR == False:
-        if state.pos[b] == 't2':
-            state.delete['t2'] = True
-        state.holdingR = b
-        state.pos[b] = 'right'
-        return state
-    elif hand == 'left' and (state.pos[b] == 't1' or state.pos[b] == 'center') and state.holdingL == False:
-        if state.pos[b] == 't1':
-            state.delete['t1'] = True
-        state.holdingL = b
-        state.pos[b] = 'left'
-        return state
-    else:
-        return False
-    
+	if hand == 'left' and state.holdingL == False and (state.pos[b] == 't1' or 
+	state.pos[b] == 'center'):
+		state.holdingL = b
+		if state.pos[b] == 't1':
+			state.clear['t1'] = True
+		state.pos[b] = 'left'
+		return state
+	elif hand == 'right' and state.holdingR == False and (state.pos[b] == 't2' or 
+	state.pos[b] == 'center'):
+		state.holdingR = b
+		if state.pos[b] == 't2':
+			state.clear['t2'] = True
+		state.pos[b] = 'right'
+		return state
+	else:
+		return False
 
+    
 def place(state, b, dest, hand):
-    if dest == 'center': 
-        if state.pos[b] == 'right':
-            state.holdingR = False
-        elif state.pos[b] == 'left':
-            state.holdingL = False
-        state.pos[b] = dest
-        return state
-    elif dest == 't1':
-        if state.pos[b] == 'left' and state.delete['t1']:
+    if dest == 't1':
+        if state.pos[b] == 'left' and state.clear['t1']:
             state.holdingL = False
             state.pos[b] = dest
-            state.delete['t1'] = False
+            state.clear['t1'] = False
             return state
     elif dest == 't2':
-        if state.pos[b] == 'right' and state.delete['t2']:
+        if state.pos[b] == 'right' and state.clear['t2']:
             state.holdingR = False
             state.pos[b] = dest
-            state.delete['t2'] = False
+            state.clear['t2'] = False
             return state
+    elif dest == 'center' and state.pos[b] == 'right':
+        state.holdingR = False
+        state.pos[b] = dest
+        return state
+    elif dest == 'center' and state.pos[b] == 'left':
+        state.holdingL = False
+        state.pos[b] = dest
+        return state
     else:
         return False
+"""
+In each planning method, the first argument is the current state (this is analogous to Python methods, in which the first argument is the class instance). The rest of the arguments must match the arguments of the task that the method is for. For example, ('pickup', b, hand) has a method get_m(state, b, hand), as shown below.
+"""
+
+# check if a block is in its final position
+def is_done(state, b, goal):
+	if (state.pos[b] == goal.pos[b]) :
+		return True
+	else : return False
 
 
+#check what state would result in moving block b
+def status(state, b, goal):
+	if is_done(state, b, goal):
+		return 'done'
+	elif goal.pos[b] == 'center' and state.pos[b] == 't1':
+		return 'move_to_center_left'
+	elif goal.pos[b] == 'center' and state.pos[b] == 't2':
+		return 'move_to_center_right'
+	elif goal.pos[b] == 't1' and state.pos[b] == 'center' and state.clear['t1']:
+        return 'move_to_t1'
+	elif goal.pos[b] == 't1' and state.pos[b] == 't2':
+        return 'move_to_center_right'
+	elif goal.pos[b] == 't2' and state.pos[b] == 'center'and state.clear['t2']:
+        return 'move_to_t2'
+	elif goal.pos[b] == 't2' and state.pos[b] == 't1':
+        return 'move_to_center_left'
+	else:
+		return 'error'
+
+	
 # methods for "move_blocks"
 def moveb_m(state, goal):
+#return [('move_one', 'blue', 'center', 'left') , ('move_one', 'green', 'center' , 'right') , ('move_one', 'green', 't1', 'left') , ('move_one', 'blue', 't2', 'right')]
     for b in state.pos:
-        if state.pos[b] == goal.pos[b]:
-            continue
-        elif goal.pos[b] == 'center' and state.pos[b] == 't1':
-            return [('move_one', ''+b, 'center', 'left'), ('move_blocks', goal)]
-        elif goal.pos[b] == 'center' and state.pos[b] == 't2':
-            return [('move_one', ''+b, 'center', 'right'), ('move_blocks', goal)]
-        elif goal.pos[b] == 't1' and state.pos[b] == 't2':
-            return [('move_one', ''+b, 'center', 'right'), ('move_blocks', goal)]
-        elif goal.pos[b] == 't2' and state.pos[b] == 't1':
-            return [('move_one', ''+b, 'center', 'left'), ('move_blocks', goal)]  
-        elif goal.pos[b] == 't1' and state.pos[b] == 'center' and state.delete['t1']:
+    	s = status(state, b, goal)
+        if s == 'done':
+    		continue
+        elif s == 'move_to_center_left':
+    		return [('move_one', ''+b, 'center', 'left'), ('move_blocks', goal)]
+        elif s == 'move_to_center_right':
+	   	   return [('move_one', ''+b, 'center', 'right'), ('move_blocks', goal)]
+        elif s == 'move_to_t1':
             return [('move_one', ''+b, 't1', 'left'), ('move_blocks', goal)]
-        elif goal.pos[b] == 't2' and state.pos[b] == 'center'and state.delete['t2']:
-            return [('move_one', ''+b, 't2', 'right'), ('move_blocks', goal)]      
+        elif s == 'move_to_t2':
+        	return [('move_one', ''+b, 't2', 'right'), ('move_blocks', goal)]
         else:
-            continue
-    return []
-
-    
-
-
+        	continue
+	return []
+	
 # methods for "move_one"
 def move1(state, b, dest, hand):
     """
@@ -132,13 +158,13 @@ def formulate_problem():
     sstate.pos['red'] = 'center'
     sstate.pos['blue'] = 't1'
     sstate.pos['green'] = 't2'
-    sstate.delete = {'t1':False, 't2':False, 'center':False}
+    sstate.clear = {'t1':False, 't2':False, 'center':False}
 
     goal = Goal('Goal') 	# Same as above but for goal instead of start state
     goal.holdingL = False
     goal.holdingR = False
     goal.pos = {'red':'center','blue':'t2', 'green':'t1'}
-    goal.delete = {'t1':False, 't2':False, 'center':False}
+    goal.clear = {'t1':False, 't2':False, 'center':False}
     # Return planner and populated state and goal
     return (sstate, goal)
 
